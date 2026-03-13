bcafb49c97c8988c25b8a91bf87b8bb9e0f95060
Bahiru <bahiru2020@gmail.com>
2026-03-13 00:37:10 -0400

    3rd commit logging modified

--- utility/settings.py
+++ utility/settings.py
@@ -120,10 +120,7 @@ MESSAGE_TAGS = {messages.ERROR: 'danger',}
 
 if DEVELOPMENT_MODE is True:
     DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
     }
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
     if os.getenv("DATABASE_URL", None) is None:
