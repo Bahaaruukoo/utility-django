4be09928001198046b381481181984bea5b3d402
Bahiru <bahiru2020@gmail.com>
2026-03-16 00:58:48 -0400

    commit signals disabled modified

--- bills/apps.py
+++ bills/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class BillsConfig(AppConfig):
     name = 'bills'
     default_auto_field = 'django.db.models.BigAutoField'
-
+    '''
     def ready(self):
-        import bills.signals
+        import bills.signals'''
